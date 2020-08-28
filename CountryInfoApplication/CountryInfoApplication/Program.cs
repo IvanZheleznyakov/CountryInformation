10b12442c1e2737dd6af1743feebdc1a60a546d0
IvanZheleznyakov <iz14@yandex.ru>
2020-08-28 22:40:42 +0300

    добавил файл конфигурации, теперь строка подключения задается там

--- CountryInfoApplication/CountryInfoApplication/Program.cs
+++ CountryInfoApplication/CountryInfoApplication/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Data.SqlClient;
 using System.IO;
 using System.Net;
+using System.Configuration;
 
 namespace CountryInfoApplication
 {
@@ -10,57 +11,66 @@ namespace CountryInfoApplication
     {
         static void Main(string[] args)
         {
-            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=CountryInfoDatabase;Integrated Security=True";
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            Console.WriteLine(connectionString);
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
             {
+                // Открываем подключение
                 connection.Open();
-                Console.WriteLine("Подключение с сервером БД открыто, можете приступать к работе.");
-                int operation = 1;
-                while (operation != 0)
+                Console.WriteLine("Подключение открыто");
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Проверьте файл конфигурации.");
+                return;
+            }
+            int operation = 1;
+            while (operation != 0)
+            {
+                UIConsole.PrintMenu();
+
+                while (!Int32.TryParse(Console.ReadLine(), out operation))
                 {
+                    Console.WriteLine("Ошибка ввода данных! Запрос должен являться целым числом. \n");
                     UIConsole.PrintMenu();
-
-                    while (!Int32.TryParse(Console.ReadLine(), out operation))
-                    {
-                        Console.WriteLine("Ошибка ввода данных! Запрос должен являться целым числом. \n");
-                        UIConsole.PrintMenu();
-                    }
-
-                    switch (operation)
-                    {
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        case 0:
-                            break;
-                        default:
-                            Console.WriteLine("Данной операции нет в списке. Попробуйте еще раз. \n");
-                            break;
-                    }
-
-                    string eur = "europe";
-                    string afc = "afc";
-                    string sqlExpr = "select Regions.Id from Regions where Regions.Name = '" + afc + "'";
-
-                    SqlCommand command = new SqlCommand(sqlExpr, connection);
-                    SqlDataReader reader = command.ExecuteReader();
-
-                    if (reader.HasRows) 
-                    {
-                        Console.WriteLine("{0}", reader.GetName(0));
-
-                        while (reader.Read()) 
-                        {
-                            object id = reader.GetValue(0);
-
-                            Console.WriteLine("{0}", id);
-                        }
-                    }
-
-                    reader.Close();
                 }
+
+                switch (operation)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        Console.WriteLine("Данной операции нет в списке. Попробуйте еще раз. \n");
+                        break;
+                }
+
+                string eur = "europe";
+                string afc = "afc";
+                string sqlExpr = "select Regions.Id from Regions where Regions.Name = '" + eur + "'";
+
+                SqlCommand command = new SqlCommand(sqlExpr, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows) 
+                {
+                    Console.WriteLine("{0}", reader.GetName(0));
+
+                    while (reader.Read()) 
+                    {
+                        object id = reader.GetValue(0);
+
+                        Console.WriteLine("{0}", id);
+                    }
+                }
+
+                reader.Close();
             }
 
             Console.WriteLine("Подключение закрыто...");
--- CountryInfoApplication/CountryInfoApplication/RestCountriesAPITools.cs
+++ CountryInfoApplication/CountryInfoApplication/RestCountriesAPITools.cs
@@ -32,7 +32,6 @@ namespace CountryInfoApplication
                 }
             }
 
-
             return result;
         }
     }
