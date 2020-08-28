d2fe925e57175858ea0e27ce7cfe2e9bf4b059a1
IvanZheleznyakov <iz14@yandex.ru>
2020-08-29 00:41:39 +0300

    появилось чуть больше неоптимизированного, некрасивого и нерабочего кода в метод добавления записи в бд

--- CountryInfoApplication/CountryInfoApplication/DatabaseTools.cs
+++ CountryInfoApplication/CountryInfoApplication/DatabaseTools.cs
@@ -31,7 +31,57 @@ namespace CountryInfoApplication
 
         public void AddCountryInfoToDatabase(string name, string countryCode, string capital, string area, string population, string region)
         {
+            string sqlSelectCapital = "SELECT Capitals.Id FROM Capitals WHERE Capitals.Name = '" + capital + "'";
+            object idCapital = null;
 
+            SqlCommand command = new SqlCommand(sqlSelectCapital, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (!reader.HasRows)
+            {
+                reader.Close();
+                string sqlInsertNewCapital = "INSERT Capitals VALUES ('" + capital + "')";
+                command = new SqlCommand(sqlInsertNewCapital, connection);
+                command.ExecuteNonQuery();
+
+                command = new SqlCommand(sqlSelectCapital, connection);
+                reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    idCapital = reader.GetValue(0);
+                }
+            }
+
+            string sqlSelectRegion = "SELECT Regions.Id FROM Regions WHERE Regions.Name = '" + region + "'";
+            object idRegion = null;
+
+            command = new SqlCommand(sqlSelectRegion, connection);
+            reader = command.ExecuteReader();
+
+            if (!reader.HasRows)
+            {
+                string sqlInsertNewCapital = "INSERT Regions VALUES ('" + region + "')";
+                /*     Console.WriteLine("{0}", reader.GetName(0));
+
+                     while (reader.Read())
+                     {
+                         object id = reader.GetValue(0);
+
+                         Console.WriteLine("{0}", id);
+                     }*/
+            }
+            else
+            {
+                while (reader.Read())
+                {
+                    idRegion = reader.GetValue(0);
+                }
+            }
+
+
+
+            reader.Close();
         }
 
         public void CloseConnection()
